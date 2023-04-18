0bd5b942847a570f74101c1c49e892bdc90ca6af
jewelrana3 <juyelrana7752@gmail.com>
2023-04-18 21:38:16 +0600

    first

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCx-KgQ3T-kqIhQZwNhcNr_qQiqLVoNoH8",
+  authDomain: "module-62-daragon-project.firebaseapp.com",
+  projectId: "module-62-daragon-project",
+  storageBucket: "module-62-daragon-project.appspot.com",
+  messagingSenderId: "96792133704",
+  appId: "1:96792133704:web:e374f36ffd7322d7fc987d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
